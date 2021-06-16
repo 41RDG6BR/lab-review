f8d50f188504a7afa635822ddbe2f0d10fb119f5
Rodrigo Nogueira <rdg6design@gmail.com>
2021-06-16 13:25:09 -0300

    React Form Validation With Formik and Yup

--- formikyup/src/App.js
+++ formikyup/src/App.js
@@ -1,15 +1,13 @@
-// import rocketImg from './assets/front.png'
-import Signup from './components/Signup'
+import { Signup } from './components/Signup';
 
 function App() {
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col-md-5">
-          Signup
-        </div>
-        <div className="col-md-7">
           <Signup />
+        </div>
+        <div className="col-md-7 my-auto">
           {/* <img className="img-fluid w-100" src={rocketImg} alt=""/> */}
         </div>
       </div>
---
+++ formikyup/src/components/Field.js
@@ -0,0 +1,7 @@
+import React from 'react'
+export const Signup = () => {
+    return (
+        <h1>ROdrigo</h1>
+    )
+}
+export default Signup
