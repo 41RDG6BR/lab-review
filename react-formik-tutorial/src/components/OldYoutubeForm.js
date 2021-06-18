0935c305982cd99d40d08056fb9463b20975312f
Rodrigo Nogueira <rdg6design@gmail.com>
2021-06-18 08:43:22 -0300

    Field Revisited

--- react-formik-tutorial/src/components/OldYoutubeForm.js
+++ react-formik-tutorial/src/components/OldYoutubeForm.js
@@ -5,7 +5,9 @@ import * as Yup from 'yup'
 const initialValues = {
     name: '',
     email: '',
-    channel: ''
+    channel: '',
+    comments: '',
+    address: ''
 }
 
 const onSubmit = (values) => {
@@ -54,8 +56,31 @@ function OldYoutubeForm() {
                     type='text' 
                     id='channel' 
                     name='channel' 
+                    placeholder='Youtube channel name'
                 />
                 <ErrorMessage name='channel' />
+            </div>
+            <div className='form-control'>
+                <label htmlFor='comments'>Comments</label>
+                <Field component='textarea' id='comments' name='comments' />
+            </div>
+
+            <div className='form-control'>
+                <label htmlFor='address'>Address</label>
+                <Field name='address'>
+                    {
+                        (props) => {
+                            const { field, form, meta } = props
+                            // console.log('Render props', props)
+                            return (
+                                <div>
+                                 {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                                 <input type='text' id='address' {...field} />
+                                </div>
+                            )
+                        }
+                    }
+                </Field>
             </div>
 
             <button type='submit'>Submit</button>
