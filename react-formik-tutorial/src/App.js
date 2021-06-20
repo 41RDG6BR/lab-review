f47d92f507cd70eb3b194dbc7dc78a8471a3628e
Rodrigo Nogueira <rdg6design@gmail.com>
2021-06-19 23:33:54 -0300

    Manually trigering validation

--- react-formik-tutorial/src/App.js
+++ react-formik-tutorial/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css'
-import OldYoutubeForm from './components/OldYoutubeForm'
+import YoutubeForm from './components/YoutubeForm'
 
 function App() {
   return (
     <div className="App">
-      <OldYoutubeForm />
+      <YoutubeForm />
     </div>
   );
 }
