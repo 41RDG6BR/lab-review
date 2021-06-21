9bdf20f38599175817c7ee3596b5cff816c7fdd4
Rodrigo Nogueira <rdg6design@gmail.com>
2021-06-21 08:28:46 -0300

    Registration Form

--- formikcontrols/src/App.js
+++ formikcontrols/src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
 // import FormikContainer from './components/FormikContainer'
-import LoginForm from "./components/LoginForm";
+// import LoginForm from "./components/LoginForm";
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <RegistrationForm />
     </div>
   );
 }
