26b6e71be08ecc0d4673c2d8c402c8eb98e16f8a
Rodrigo Nogueira <rdg6design@gmail.com>
2021-06-21 02:47:39 -0300

    Building Reusable Formik Controls

---
+++ formikcontrols/src/components/FormikContainer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+
+function FormikContainer() {
+    const initialValues = {}
+
+    const validationSchema = Yup.object({})
+    const onSubmit = values => console.log('Form data', values)
+
+    return (
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onsubmit}>
+            {
+                formik => <Form>
+                    <submit type='submit'>Submit</submit>
+                </Form>
+            }
+        </Formik>
+    )
+}
+
+export default FormikContainer
