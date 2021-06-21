f390790d701eb24846e4a55d1be6aad3f47657cf
Rodrigo Nogueira <rdg6design@gmail.com>
2021-06-21 04:36:45 -0300

    Select

--- formikcontrols/src/components/FormikControl.js
+++ formikcontrols/src/components/FormikControl.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Input from './Input'
+import Select from './Select'
 import Textarea from './Textarea'
 
 function FormikControl(props) {
@@ -7,10 +8,13 @@ function FormikControl(props) {
     const{ control, ...rest } = props
 
     switch(control) {
-        case 'input': return <Input {...rest} />
-        case 'textarea': return<Textarea {...rest}/>
-        case 'select':
-        case 'radio':
+        case 'input': 
+            return <Input {...rest} />
+        case 'textarea': 
+            return <Textarea {...rest}/>
+        case 'select': 
+            return <Select {...rest}/>
+        case 'radio': 
         case 'checkbox':
         case 'date':
         default: return null
